50832f91b0c389499a863e25e9c8d46d78400ed5
dpp17 <durga123q.com>
2023-04-15 04:41:23 +0530

    Added:: Validating gender,startDate,profilePic,note and deparment

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.NonNull;
 import lombok.ToString;
 import java.time.LocalDate;
 import java.util.List;
@@ -17,13 +17,20 @@ public class EmployeeDTO {
     @Min(value = 500, message = "Salray shoild be greater than 500")
     public double salary;
 
+    @Pattern(regexp = "male|female", message = "Gender need to be male or female")
     public String gender;
 
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "startDate should not be empty")
+    @PastOrPresent(message = "startDate should be past or today's date")
     public LocalDate startDate;
 
+    @NotBlank(message = "profilePic cannot be Empty")
     public String profilePic;
 
+    @NotBlank(message = "Note cannot be Empty")
     public String note;
 
+    @NotNull(message = "department shouldn't be Empty")
     public List<String> department;
 }
